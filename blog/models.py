d78b2eddb5a313f3b98910d3be3408544913b77c
drewkb <drewkb@mail.ru>
2017-01-12 18:42:59 +0300

    fixed publish() method in post model

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     published = models.DateTimeField(blank=True, null=True)
     
     def publish(self):
-        self.published_date = timezone.now()
+        self.published = timezone.now()
         self.save()
 
     def __str__(self):
